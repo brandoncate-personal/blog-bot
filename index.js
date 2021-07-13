a3abc4a504d646ac369dfd8c3a0097a63c20c74f
Brandon Cate <brandoncate95@gmail.com>
2021-07-12 20:20:09 -0500

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = robot => {
+    robot.on('issues.opened', async context => {
+        const { body } = context.payload.issue;
+
+        // create a comment
+        const params = context.issue({
+            body: body.includes('Thanks')
+                ? 'You are Welcome!'
+                : 'Thanks!'
+        });
+        // publish it
+        return context.octokit.issues.createComment(params);
+    });
+}
